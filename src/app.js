c6bdabf6f648223efca1d8863862b9b0997695a8
jefti <jeftimeira@gmail.com>
2023-08-09 14:32:18 -0300

    feature: post ping

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import router from "./routes/index.route.js";
 
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
--- src/routes/authorization.route.js
+++ src/routes/authorization.route.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
 
 const authorizationRouter = Router();
-
+    authorizationRouter.post('/ping', (req, res)=>{
+        console.log('ping...');
+        res.status(200).send('ping');
+    });
 export default authorizationRouter;
